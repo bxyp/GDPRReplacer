30d8093ae332555f73f28b9ea7ec602b666f0923
Bryan Xay <bxydev@gmail.com>
2023-11-27 20:37:01 +0000

    Added a dynamic number value to the GDPR Consent

--- gdpr.py
+++ gdpr.py
@@ -1,15 +1,16 @@
 #main function to replace the GDPR
 def replace_placeholders(url):
+  dynamic_number =  68
   # Replace placeholders in the URL
   replaced_url = url.replace("${GDPR}", "#{request.keyValue('_fw_gdpr')}") \
-                     .replace("${GDPR_CONSENT_68}", "#{request.keyValue('_fw_gdpr_consent')}")
+                     .replace("${GDPR_CONSENT_" + str(dynamic_number) + "}", "#{request.keyValue('_fw_gdpr_consent')}")
   return replaced_url
 
 
 def main():
   while True:
     # Get URL input from the user
-    user_input = input("/nEnter a URL (type 'end' to exit): ")
+    user_input = input("\nEnter a URL (type 'end' to exit): \n")
 
     # Check if the user wants to end the program
     if user_input.lower() == 'end':
@@ -17,7 +18,7 @@ def main():
 
     # Replace placeholders and display the result
     result_url = replace_placeholders(user_input)
-    print("\nModified URL:", result_url)
+    print("\nModified URL:\n", result_url)
     print()
 
 #calling main
